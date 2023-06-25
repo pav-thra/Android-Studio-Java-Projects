f0223868e141d248111ebfb662c561b208959858
Pavithra <v.pavithrav1011@gmail.com>
2023-06-25 23:09:33 +0530

    Using Java

--- DatenTimePicker/app/src/main/java/com/example/datentimepicker/MainActivity.java
+++ DatenTimePicker/app/src/main/java/com/example/datentimepicker/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.datentimepicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+Button db,tb;
+EditText dt,tt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db=findViewById(R.id.date_b);
+        tb=findViewById(R.id.time_b);
+        dt=findViewById(R.id.date_pt);
+        tt=findViewById(R.id.time_pt);
+
+        db.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c=Calendar.getInstance();
+                int y,m,d;
+                y=c.get(Calendar.YEAR);
+                m=c.get(Calendar.MONTH);
+                d=c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dp=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        dt.setText(i2+"/"+(i1+1)+"/"+i);
+                    },y,m,d);
+                dp.show();
+                });
+            }
+        });
     }
 }
